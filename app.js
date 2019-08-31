853c3e50483d03d5a9b0f4ebd91296545c8fd3bb
Mishan Madhupa <mishancool2016@gmail.com>
2019-08-31 08:18:04 +0530

    Require express,body-parser and cors, Set middleware and Start server

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 require('./config/config.js');
 require('./models/db');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+var app = express();
+
+//middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+//start server
+app.listen(process.env.PORT, () => console.log(`Server start at port: ${process.env.PORT}`));
