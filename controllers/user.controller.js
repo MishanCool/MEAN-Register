0d06b471d6960d36c6b8e50f83da9bbca5598f5f
Mishan Madhupa <mishancool2016@gmail.com>
2019-08-31 23:28:05 +0530

    Create register function to handel request from client side

---
+++ controllers/user.controller.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+//function name register
+// this function use to handel request from client side
+module.exports.register = (req, res, next) =>{
+    console.log('inside register fn...!');
+}
