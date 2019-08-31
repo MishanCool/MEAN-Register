b73a5789f41bde93ffb998a90c467ad2be7a9a71
Mishan Madhupa <mishancool2016@gmail.com>
2019-08-31 23:41:33 +0530

    Require index.router.js file and create middleware

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const rtsIndex = require('./routes/index.router');
+
 var app = express();
 
 //middleware
 app.use(bodyParser.json());
 app.use(cors());
+app.use('/api', rtsIndex);//configer the middleware
 
 //start server
 app.listen(process.env.PORT, () => console.log(`Server start at port: ${process.env.PORT}`));
