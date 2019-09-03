5d053acac60d0434b59b10b5b4ddc70e59f55373
Mishan Madhupa <mishancool2016@gmail.com>
2019-09-03 09:13:22 +0530

    Generate a salt and hash on separate function calls

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');//generate the random saltSecret, first require the bcryptjs state ment
 
 var userSchema = new mongoose.Schema({
   username: String,
@@ -6,5 +7,21 @@ var userSchema = new mongoose.Schema({
   password: String,
   salt: String // automaticaly create user registration function in user.controller.js file
 }, {timestamps: true});
+
+
+//Event
+userSchema.pre("save", function(next) {
+
+  //Generate a salt and hash on separate function calls
+  bcrypt.genSalt(10, function(err, salt) {                     //Randomly jenarate  result secret code from bcryptjs
+    bcrypt.hash(this.Password, salt, function(err, hash) {
+        // Store hash in your password DB.
+        this.password = hash; //encripted password inside this hash parameter, save inside the password property in mongo db.
+        this.saltSecret = salt;
+        next(); // exicute save user recode operation in user.controller.js file
+    });
+  });
+});
+
 
 mongoose.model('User', userSchema); //('model name' , schema object)
