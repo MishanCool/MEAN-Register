3842a22e420ed10d6df37e82f0f82a92f13cdde7
Mishan Madhupa <mishancool2016@gmail.com>
2019-09-03 12:09:40 +0530

    Code error handler function in app.js file

--- app.js
+++ app.js
@@ -14,5 +14,14 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use('/api', rtsIndex);//configer the middleware
 
+//error handler
+app.use((err, req, res, next) =>{
+    if(err.name === 'ValidationError'){
+        var valErrors = [];
+        Object.keys(err.errors).forEach(key => valErrors.push(err.errors[key].message));
+        res.status(422).send(valErrors)
+    }
+});
+
 //start server
 app.listen(process.env.PORT, () => console.log(`Server start at port: ${process.env.PORT}`));
