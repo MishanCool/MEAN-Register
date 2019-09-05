8abd471b85938c9c31969edb4f78d030449bcd32
Mishan Madhupa <mishancool2016@gmail.com>
2019-09-05 06:42:25 +0530

    Import user.model class and code User object

--- client/angularWeb/src/app/shared/user.service.ts
+++ client/angularWeb/src/app/shared/user.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
 
+
+import { User } from './user.model';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  selectedUser: User = {       // User -> object
+    username: '',              // Declair user model object
+    email: '',                 //
+    password: ''               //
+  };
 
   constructor() { }
 }
