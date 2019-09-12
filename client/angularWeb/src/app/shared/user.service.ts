24ec93ab2f75ad0b33f41b43b11685946e7e57c9
Mishan Madhupa <mishancool2016@gmail.com>
2019-09-12 22:39:51 +0530

    Import environment which create in environment.ts file

--- client/angularWeb/src/app/shared/user.service.ts
+++ client/angularWeb/src/app/shared/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
+import { environment } from '../../environments/environment';
 import { User } from './user.model';
 
 @Injectable({
