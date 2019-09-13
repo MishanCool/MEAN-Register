7dbdaec4b55bf3973e54e9f0aa0c295b5fbfac8b
Mishan Madhupa <mishancool2016@gmail.com>
2019-09-13 22:35:57 +0530

    COde onSubmit function res => part and Set showSucessMessage as boolean

--- client/angularWeb/src/app/components/user/sign-up/sign-up.component.ts
+++ client/angularWeb/src/app/components/user/sign-up/sign-up.component.ts
@@ -13,13 +13,30 @@ import { UserService } from './../../../shared/user.service';
 export class SignUpComponent implements OnInit {
   emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 
+  showSucessMessage: boolean;
+  // serverErrorMessages: string;
+
+
   constructor(private userService: UserService) { }
 
   ngOnInit() {
   }
 
   onSubmit(form: NgForm) {
-
+    this.userService.postUser(form.value).subscribe(
+      res => {
+        this.showSucessMessage = true;
+        setTimeout(() => this.showSucessMessage = false, 4000);
+        this.resetForm(form);
+      },
+      // err => {
+      //   if (err.status === 422) {
+      //       this.serverErrorMessages = err.error.join('<br/>');
+      //   } else {
+      //       this.serverErrorMessages = 'something went wrong. Please contact admin';
+      //   }
+      // }
+    );
   }
 
 }
